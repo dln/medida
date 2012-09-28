865a600e4f1c35a47e5136b04d716db9e3b735f5
Daniel Lundin <dln@spotify.com>
2012-09-28 17:32:54 +0200

    Add Debian packaging

--- libmedida.pc.cmake
+++ libmedida.pc.cmake
@@ -3,7 +3,7 @@ exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=${prefix}/@LIB_INSTALL_DIR@
 includedir=${prefix}/@INCLUDE_INSTALL_ROOT_DIR@
 
-Name: medida
+Name: libmedida
 Description: Medida metrics library for C++
 Version: @medida_VERSION@
 Libs: -L${libdir} -lmedida
